202ff25d53130c1a00b855654917b7994501046d
ENG10048 Protocolos de Comunicacao - A - Grupo 3 <eng10048_3@routh.ece>
2018-07-05 15:26:59 -0300

    Mudanças Planta: Adição gráfico.

--- src/server.c
+++ src/server.c
@@ -4,17 +4,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h> 
+#include <math.h>
+#include <sys/types.h> 	
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include "bib_graf.c"
+#include <pthread.h>
 
 #define clear() printf("\033[H\033[J")
 
+/*
+ * Server: ang entrada ang saida  e nivel... 
+ * cliente(CLP)  ang entrada e  nivel (calculados pelo controle...)
+ *
+ */ 
 int per_sim = 0;
 int max = 50;
 int valvula = 0;
 int nivel = 100;
 int simulacao=0;
+double t=0;
+double pi=3.1415926;
 
 
 typedef struct data_struct {
@@ -22,11 +32,17 @@ typedef struct data_struct {
   double controle[20];
   double tempo[20];
   double level[20]; //Nivel do tanque...
+  double inangle[20]; // Angulo de entrada da válvula?
+  double outangle[20]; // Angulo de saída da válvula?
   double valor; //valor que vem da comucicacao IP. Valor da abertura da valvula
+
+  int consumo; //Primeira info
   int atualizar; //flag para atualizar o valor da valvula. 1-AbreValvula 2-FechaValvula
 } Data;
 
 Data dados;
+
+// Fim das declarações.. inicio das 
 
 int mili(int milisec){
   struct timespec req = {0};
@@ -54,8 +70,8 @@ void chamar(char *str,int valor,int qtd,char *print){
   }
   if(strcmp(str,"setConsumo")==0)
   {
-    max = valor;
-    sprintf(print,"%s%d!",print,max);
+    dados.consumo = valor;
+    sprintf(print,"%s%d!",print,dados.consumo);
     find=1;
   }
   if(strcmp(str,"iniciaSimulacao")==0)
@@ -126,6 +142,39 @@ void ident(char *str,char *print){
 }
 
 
+
+// **** N SEI Q Q A FUNCAO OUTANGLE FAZ
+double outangle(double t) {
+   if(t <= 0) {
+     return 50;
+    }
+    if(t<20000) {
+      return (50+t/400);
+    }
+    if(t<30000) {
+      return 100;
+    }
+    if(t<50000) {
+      return (100-(t-30000)/250);
+    }
+    if(t<70000) {
+      return (20 + (t-50000)/1000);
+    }
+    if(t<100000) {
+      return (40+20*cos((t-70000)*2*pi/10000));
+    }
+    return 100;
+}
+
+
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+
+
 void IPServer(char *port){
      int sockfd, newsockfd, portno,sair=0;
      socklen_t clilen;
@@ -175,24 +224,21 @@ void IPServer(char *port){
      close(sockfd);
 }
 
-void Graph(){
-  
-}
 
-void Plant(){
+void *plant(){
   int dT=1, T=0 ,i=0;
-  double delta=0, inangle[20], outangle[20];
+  double delta=0, influx, outflux;
   
   //MUTEX ON
   dados.level[0]=0.4;
+
   //MUTEX OFF
- 
+  dados.inangle[0]=0;
   
   //** INICIO LOOP INFINITO**
   while(1){   
-    for(i=0;i<20;i++){ 
-     
-    mili(10);
+   
+    for(i=0;i<19;i++){    
       
     //**** INICIO MUTEX1 PLANTA***
       if(dados.atualizar==1) { //Abre Valvula
@@ -205,88 +251,107 @@ void Plant(){
       }
     //****FIM MUTEX1 PLANTA***
     
-   /* 
+      
+     mili(10); // Executa a rotina da planta a cada 10ms... 
+
       if(delta > 0) {
 	if(delta < 0.02*dT){
-	    in.angle(T+dT)= in.angle(T)+delta
-	    delta = 0 
+	    dados.inangle[T+1]= dados.inangle[T]+delta;
+	    delta = 0;
 	} else{
-	    in.angle(T+dT)=  in.angle(T)+0.02*dT
-	    delta -=  0.02*dT 
+	    dados.inangle[T+1]= dados.inangle[T]+0.02*dT;
+	    delta -=  0.02*dT; 
 	    }
       }else if(delta < 0){  
 	if(delta > -0.02*dT){
-	    in.angle(T+dT)= in.angle(T)+delta
-	    delta = 0
+	    dados.inangle[T+1]= dados.inangle[T]+delta;
+	    delta = 0;
 	}else{
-	    in.angle(T+dT)=  in.angle(T)-0.02*dT
-	    delta +=  0.02*dT
+	    dados.inangle[T+1]=  dados.inangle[T]-0.02*dT;
+	    delta +=  0.02*dT;
 	}
       }
-    
-
-      influx = 1*sin(pi/2*in.angle/100)
-      outflux= (MAX/100)*(level(T)/1.25+0.2)*sin(pi/2*out.angle/100)
+         
+      t = t+T;
+      dados.tempo[i]=t;
+      dados.outangle[i]=outangle(t);
+      influx = 1*sin(pi/2*dados.inangle[T]/100);
+      outflux= (dados.consumo/100)*(dados.level[T]/1.25+0.2)*sin(pi/2*dados.outangle[i]/100); //outangle é uma função...
 
       //**** INICIO MUTEX2 PLANTA***
-      level(0)=0.4
-      level(T+dT)=level(T)+0.00001*dT*(influx-outflux)
-      //**** FIM MUTEX2 PLANTA****/
+      //dados.level[0]=0.4;
+      dados.level[T+1]=dados.level[T]+0.00001*dT*(influx-outflux);
+      //**** FIM MUTEX2 PLANTA**
+    //  printf("%lf %lf %lf %lf \n", dados.level[T], dados.inangle[T], outangle(t), delta);
+
+      T++;
     }
-    // in.angle(0)=in.angle(19); //Reseta o vetor, ultimo valor é o primeiro
-    
+
+    dados.inangle[0]=dados.inangle[19]; //Reseta o vetor, ultimo valor é o primeiro   
+    dados.level[0]=dados.level[19];
     T=0; // Fim do FOR, somente reseta o vetor pra salvar só 20 posições....
-
+    quitevent();  
+   }
 }
+
 //** FIM LOOP INFINITO*
-
-
-
 //dT em miliseconds <=> setPeriodo
 //MAX <=> setConsumo
 //===================================
 
-// **** N SEI Q Q A FUNCAO OUTANGLE FAZ
 
-//   double outangle(double t) {
-//     if(t <= 0) {
-//       return 50;
-//     }
-//     if(t<20000) {
-//       return (50+t/400);
-//     }
-//     if(t<30000) {
-//       return 100;
-//     }
-//     if(t<50000) {
-//       return (100-(t-30000)/250);
-//     }
-//     if(t<70000) {
-//       return (20 + (t-50000)/1000);
-//     }
-// 
-//     if(t<100000) {
-//       return(40+20*cos((t-70000)*2*pi/10000));
-//     }
-//     return 100;
-//   }
-
+void *graph(){ //void *arg){
+  Tdataholder *data;
+  int i,j=0;
+  double time_offset=0;
+  
+  data = datainit(640,480,55,110,45,0,0);
+/*
+  for (t=0;t<55;t+=0.1) {
+    datadraw(data,t,(double)(50+20*cos(t/5)),(double)(70+10*sin(t/10)),(double)(20+5*cos(t/2.5)));
+  }*/
+  
+  while(1){
+    for(j=0;j<20;j+=5){
+      mili(50);
+      for(i=j;i<(j+5);i++){
+	//printf("%d \n", dados.level[i]);
+	datadraw(data,dados.tempo[i],dados.level[i],dados.outangle[i],dados.inangle[i]);  //dados.tempo[i]-time_offset,dados.controle[i],dados.valor[i],dados.saida[i]);
+	if((dados.tempo[i]-time_offset)>=55){
+	  time_offset+=55;
+	  data = datainit(640,480,55,110,45,0,0);
+	}
+      }
+      quitevent();
+    }
+  }
 }
 
-
-
-void error(const char *msg)
-{
-    perror(msg);
-    exit(1);
-}
 
 int main(int argc, char *argv[])
 {
+    pthread_t thread_graf, thread_plant;
     //thread ip server..
-     IPServer(argv[1]);
-     
-    //Planta
+   // IPServer(argv);
+      
+    dados.consumo = 50;
+    dados.valor = 50;
+    dados.atualizar = 1;
+    
+   // plant();
+   // graph();	
+    
+   
+    pthread_create(&thread_graf, NULL, graph, NULL);
+    pthread_create(&thread_plant, NULL, plant, NULL);
+  
+      
+    pthread_join(thread_graf, NULL);
+    pthread_join(thread_plant, NULL);
+
+    exit(0);
+    
+    
      
     //thread do grafico
 }
